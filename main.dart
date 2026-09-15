beb2bfc37c1e9a1dbc41727f944980ea00e96c9e
kirillmehopov-design <kirillmehopov@gmail.com>
2026-09-15 18:50:37 +0500

    Add Audiobook and EBook classes

--- main.dart
+++ main.dart
@@ -18,4 +18,40 @@ mixin Downloadable {
   }
 }
 
+class Audiobook extends MediaItem with Downloadable {
+  double durationHours;
+  String narrator;
+
+  Audiobook({
+    required super.id,
+    required super.title,
+    required super.price,
+    required this.durationHours,
+    required this.narrator,
+  });
+
+  @override
+  String getDetails() {
+    return 'Audiobook: $title, Narrator: $narrator, Duration: $durationHours hours, Price: $price';
+  }
+}
+
+class EBook extends MediaItem with Downloadable {
+  double fileSizeMB;
+  String author;
+
+  EBook({
+    required super.id,
+    required super.title,
+    required super.price,
+    required this.fileSizeMB,
+    required this.author,
+  });
+
+  @override
+  String getDetails() {
+    return 'EBook: $title, Author: $author, Size: $fileSizeMB MB, Price: $price';
+  }
+}
+
 void main() {}
